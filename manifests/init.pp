5e7bfab0a934f6d73f59b8c59cf461c466893588
Andrew Nash <akahadaka@gmail.com>
2014-03-27 17:05:11 +0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,59 @@
+class phalcon::install {
+    file { '/tmp/install-phalcon.sh':
+		ensure => file,
+		source => 'puppet:///modules/phalcon/install-phalcon.sh',
+		group => vagrant,
+		owner => vagrant,
+		mode => 777,
+	}
+
+	notify { 'Phalcon script has been installed successfully' : 
+		require => File['/tmp/install-phalcon.sh']
+	}
+
+	exec { 'install-phalcon-sh':
+		command => '/tmp/install-phalcon.sh',
+		require => File['/tmp/install-phalcon.sh'],
+		provider => 'shell',
+		unless => '/bin/ls -a /usr/lib/php5/* | /bin/grep -c phalcon.so',
+	}
+
+    file { '/etc/php5/conf.d/phalcon.ini':
+        content => 'extension=phalcon.so',
+        require => Exec['install-phalcon-sh']
+    }
+
+    file { '/etc/php5/cli/phalcon.ini':
+        ensure => link,
+        target => 'etc/php5/conf.d/phalcon.ini'
+    }
+
+    file { '/etc/php5/fpm/phalcon.ini':
+        ensure => link,
+        target => 'etc/php5/conf.d/phalcon.ini'
+    }
+
+    notify { 'Phalcon ini extension has been written sucessfully' : 
+		require => File['/etc/php5/conf.d/phalcon.ini']
+	}
+
+	file { '/usr/bin/phalcon':
+        ensure => link,
+        target => '/home/vagrant/phalcon-devtools/phalcon.php',
+        require => Exec['install-phalcon-sh'],
+        notify => Service['php5-fpm']
+    }
+
+    notify { 'Phalcon DevTools added to path' : 
+		require => File['/usr/bin/phalcon']
+	}
+
+}
+
+class phalcon::configure {
+}
+
+class phalcon {
+    include phalcon::install
+    include phalcon::configure
+}
