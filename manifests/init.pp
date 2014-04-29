722f0d5a30cb53052838062f55dbeaa57e596b41
Andrew Nash <akahadaka@gmail.com>
2014-04-29 17:46:07 +0200

    Using less shell script

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,43 +1,38 @@
 class phalcon::install {
     file { '/tmp/install-phalcon.sh':
-		ensure => file,
-		source => 'puppet:///modules/phalcon/install-phalcon.sh',
-		group => vagrant,
-		owner => vagrant,
-		mode => 777,
-	}
+        ensure => file,
+        source => 'puppet:///modules/phalcon/install-phalcon.sh',
+        group => vagrant,
+        owner => vagrant,
+        mode => 777,
+    }
 
-	notify { 'Phalcon script has been installed successfully' : 
-		require => File['/tmp/install-phalcon.sh']
-	}
+    notify { 'Phalcon script has been installed successfully' : 
+            require => File['/tmp/install-phalcon.sh']
+    }
 
-	exec { 'install-phalcon-sh':
-		command => '/tmp/install-phalcon.sh',
-		require => File['/tmp/install-phalcon.sh'],
-		provider => 'shell',
-		unless => '/bin/ls -a /usr/lib/php5/* | /bin/grep -c phalcon.so',
-	}
+    exec { 'install-phalcon-sh':
+        command => '/tmp/install-phalcon.sh',
+        require => File['/tmp/install-phalcon.sh'],
+        provider => 'shell',
+        unless => '/bin/ls -a /usr/lib/php5/* | /bin/grep -c phalcon.so',
+    }
 
-    file { '/etc/php5/conf.d/phalcon.ini':
+    file { '/etc/php5/mods-available/phalcon.ini':
         content => 'extension=phalcon.so',
         require => Exec['install-phalcon-sh']
     }
 
-    file { '/etc/php5/cli/phalcon.ini':
-        ensure => link,
-        target => 'etc/php5/conf.d/phalcon.ini'
-    }
-
-    file { '/etc/php5/fpm/phalcon.ini':
-        ensure => link,
-        target => 'etc/php5/conf.d/phalcon.ini'
+    exec { 'enable-phalcon-extension':
+        command => '/usr/bin/sudo php5enmod phalcon',
+        require => File['/etc/php5/mods-available/phalcon.ini']
     }
 
     notify { 'Phalcon ini extension has been written sucessfully' : 
-		require => File['/etc/php5/conf.d/phalcon.ini']
-	}
+        require => File['/etc/php5/mods-available/phalcon.ini']
+    }
 
-	file { '/usr/bin/phalcon':
+    file { '/usr/bin/phalcon':
         ensure => link,
         target => '/home/vagrant/phalcon-devtools/phalcon.php',
         require => Exec['install-phalcon-sh'],
@@ -45,8 +40,8 @@ class phalcon::install {
     }
 
     notify { 'Phalcon DevTools added to path' : 
-		require => File['/usr/bin/phalcon']
-	}
+	require => File['/usr/bin/phalcon']
+    }
 
 }
 
